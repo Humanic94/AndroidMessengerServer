f8cf47f3d058cc3d3a0a8b2a5cf44cd7f21c80ea
Jørgen Nikolai Havnsund <Jorgen.havnsund@live.no>
2016-10-13 01:06:31 +0200

    POST, PUT, DELETE, GET API implementation for message

--- src/main/java/ChatServer/resources/MessageResource.java
+++ src/main/java/ChatServer/resources/MessageResource.java
@@ -8,7 +8,11 @@ package ChatServer.resources;
 import ChatServer.Domain.Message;
 import ChatServer.service.MessageService;
 import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,20 +23,41 @@ import javax.ws.rs.core.MediaType;
  * @author Jorgen
  */
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
     MessageService messageService = new MessageService();
     
     //Get all messages
     @GET
-    @Produces("Application/json")
     public List<Message> getMessages(){
         return messageService.getAllMessages();
+    }
+    
+    //Adds a new message 
+    @POST
+    public Message addMessage(Message message){
+        return messageService.addMessage(message);
+    }
+    
+    //Changes an existing message
+    @PUT
+    @Path("/{messageId}")
+    public Message updateMessage(@PathParam("messageId") Long messageId, Message message){
+        message.setMessageId(messageId);
+        return messageService.updateMessage(message);
+    }
+    
+    //Removes a message by id
+    @DELETE
+    @Path("/{messageId}")
+    public void deleteMessage(@PathParam("messageId") Long messageId){
+        messageService.removeMessage(messageId);
     }
     
     //Search messages by Id
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_XML)
     public Message getMessage(@PathParam("messageId") Long messageId){
     return messageService.getMessage(messageId);
 
